22a708cff93c47bc713dd474738a5dce57ffb4b8
clint <clint.fleetwood@gmail.com>
2020-02-17 23:39:25 -0500

    created the app

---
+++ config.js
@@ -0,0 +1,4 @@
+const appId = '69d874382a7e6753c7ab9a6f79d6f431';//from signup on openWeathermap.org
+const proxy = 'https://cors-anywhere.herokuapp.com/';//cors is not allowed with this api. A proxy needs to be used.
+export const baseUrl = `${proxy}http://api.openweathermap.org/data/2.5/weather/?appId=${appId}`;
+export const iconUrl = 'http://openweathermap.org/img/wn/';
---
+++ index.js
@@ -0,0 +1,187 @@
+/*
+    A lot of the "appendChild" and "createElement" could be handled with string literals, 
+    but this explains DOM manipulation a bit better than innerHTML.
+*/
+
+import { baseUrl, iconUrl } from "./config.js";
+const form = document.getElementById("location");
+const content = document.getElementById("content");
+const submitButton = document.getElementById("submit");
+const errors = document.getElementById("errors");
+//const cards = [];//even though it is a const, you can still add to the array
+
+//create the functions
+const createWeatherCard = (result) => {
+    console.log(result);
+    errors.innerHTML = "";
+
+    let card = document.createElement("div");
+    card.setAttribute("class", "weather-card");
+    let title = document.createElement("h3");
+    title.appendChild(document.createTextNode(result.name));
+    card.appendChild(title);
+
+    let close = document.createElement("button");
+    close.setAttribute("class", "close");
+    close.appendChild(document.createTextNode("X"));
+    title.appendChild(close);
+    close.addEventListener("click", ()=>{
+        content.removeChild(card);
+    });
+
+    let cardContent = document.createElement("div");
+    cardContent.setAttribute("class", "card-content");
+    card.appendChild(cardContent);
+
+    //handle images
+    let imageContainer = document.createElement("div");
+    imageContainer.setAttribute("class", "image-container");
+    //result.weather is an array. I've only seen it with one, 
+    //so im just going with the first
+    let weather = result.weather[0];
+    if (weather) {
+        let imageDesc = document.createElement("label");
+        imageDesc.appendChild(document.createTextNode(weather.description));
+
+        let imgUrl = `${iconUrl}${weather.icon}@2x.png`;
+        let image = new Image();
+        image.src = imgUrl;
+
+        let imageBox = document.createElement("div");
+        imageBox.setAttribute("class", "image-box");
+
+        imageBox.appendChild(imageDesc);
+        imageBox.appendChild(image);
+
+        imageContainer.appendChild(imageBox);
+    }
+
+    cardContent.appendChild(imageContainer);
+
+    //handle tempeture
+    let tempContainer = document.createElement("div");
+    tempContainer.setAttribute("class", "temps")
+
+    let tempNow = document.createElement("div");
+    let temp;
+    let curTemp = result.main.temp;
+    if (curTemp <= 32) {
+        temp = "freezing";
+    } else if (curTemp > 32 && curTemp <= 55) {
+        temp = "cold";
+    } else if (curTemp > 55 && curTemp <= 70) {
+        temp = "comfortable"
+    } else if (curTemp > 70 && curTemp <= 90) {
+        temp = "hot";
+    } else {
+        temp = "hell";
+    }
+
+    tempNow.setAttribute("class", `current-temp ${temp}`);
+    tempNow.appendChild(document.createTextNode(Math.round(curTemp)));
+    tempContainer.appendChild(tempNow);
+
+    //add high and low
+    let high = document.createElement("div");
+    high.setAttribute("class", "high");
+    high.appendChild(document.createTextNode(`High : ${Math.round(result.main.temp_max)}`));
+    let low = document.createElement("div");
+    low.setAttribute("class","low");
+    low.appendChild(document.createTextNode(`Low : ${Math.round(result.main.temp_min)}`));
+    
+
+    tempContainer.appendChild(high);
+    tempContainer.appendChild(low);
+    cardContent.appendChild(tempContainer);
+    return card;
+
+};
+
+const resetForm = () => {
+    form.childNodes.forEach(node => {
+        if (node.tagName === "INPUT") node.value = null;
+    })
+};
+
+const updatePage = (result) => {
+
+    //if there is an error handle it.
+    if (result.hasOwnProperty("error")) {
+        let error = document.createElement("div");
+        error.setAttribute("class", "error");
+
+        //using a switch in case there are several error codes.
+        //but there is just the one right now.        
+        switch (result.error) {
+            case 'noCityOrZip':
+                error.appendChild(
+                    document.createTextNode("A city or a zip code is required.")
+                );
+                break;
+            default:
+                error.appendChild(document.createTextNode(result.error));
+        }
+        errors.appendChild(error);
+        return;
+    }
+
+    let card = createWeatherCard(result);
+    //cards.push(card);
+    content.appendChild(card);
+
+    resetForm();
+}
+
+const buildQuery = () => {
+    let city = form.querySelector("#city").value;
+    let state = form.querySelector("#state").value;
+    let zip = form.querySelector("#zip").value;
+    let units = 'imperial';
+
+    if (!(city && state) && !zip) return false;
+
+    let queryString = "&";
+    if (city) queryString += `q=${city}`;
+    if (city && state) queryString += `,${state}`;
+    queryString += ",us";
+    if (zip) queryString += `&zip=${zip},us`;
+    queryString += `&units=${units}`;
+    return queryString;
+
+    /*
+    This would be how I write it normally, but it is complex to explain
+    `${city ? city : ""}${state ? `,${state}` :""},us${zip? `&zip=${zip},us` :""}&units=${units}`;
+    */
+}
+
+const findWeather = () => {
+
+    let queryString = buildQuery();
+    if (!queryString) {
+        updatePage({ "error": "noCityOrZip" });
+        return;//prevent fetch
+    }
+
+    let fetchOptions = {
+        method: "GET",
+        mode: "cors",
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    }
+
+    fetch(`${baseUrl}${queryString}`, fetchOptions)
+        .then(result => result.json())
+        .then(json => {
+            //just in case json strings it later
+            if (Number(json.cod) === 200) {
+                updatePage(json);
+                return;
+            }
+            updatePage({ error: json.message });
+        })
+        .catch(err => updatePage({ error: err }))
+};
+
+//add the event listeners
+submitButton.addEventListener("click", findWeather);
